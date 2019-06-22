daebf780fdb894797cc2691d2b47182f87d141c9
brunoTrind4d3 <b.l.trindade@outlook.com>
2019-06-22 00:41:48 -0300

    Criação do projeto de reconhecimento facial

---
+++ treinamento.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+import numpy as np
+
+eigenface = cv2.face.EigenFaceRecognizer_create(num_components=50)
+fisherface = cv2.face.FisherFaceRecognizer_create()
+lbph = cv2.face.LBPHFaceRecognizer_create()
+
+
+def getImageId():
+    caminhos = [os.path.join('fotos', f) for f in os.listdir('fotos')]
+    faces = []
+    ids = []
+    for caminhoImagem in caminhos:
+        imagemFace = cv2.cvtColor(cv2.imread(caminhoImagem), cv2.COLOR_BGR2GRAY)
+        id = int(os.path.split(caminhoImagem)[-1].split('.')[1])
+        ids.append(id)
+        faces.append(imagemFace)
+    return np.array(ids), faces
+
+
+ids, faces = getImageId()
+
+print("Treinando...")
+eigenface.train(faces, ids)
+eigenface.write('classificadorEigen.yml')
+
+fisherface.train(faces, ids)
+fisherface.write('classificadorFisher.yml')
+
+lbph.train(faces, ids)
+lbph.write('classificadorLBPH.yml')
+
+print("Treinamento realizado")
